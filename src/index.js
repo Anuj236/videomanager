3a013abf0545ebcacc92c43371c4026e8d70f982
Anuj Kumar <kumaranujam1009@gmail.com>
2023-12-18 00:30:10 +0530

    middlewares

--- src/index.js
+++ src/index.js
@@ -11,8 +11,17 @@ dotenv.config(
         path:"./env"
     }
 );
-
-connectDB();
+ // Asynchronous function returns Promise
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => 
+    {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!", err);
+})
 
 /*const app = express();
 
