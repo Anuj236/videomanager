b4183cc9c861de6fd291f9a82dc60b3f5fc0cb34
Anuj Kumar <kumaranujam1009@gmail.com>
2023-12-19 11:44:01 +0530

    multer configuration

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -25,6 +25,4 @@ cloudinary.config({
   }
 
 
-  cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+  export { uploadOnCloudinary };
